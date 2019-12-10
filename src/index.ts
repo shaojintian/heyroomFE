f566a18d88c3776ff3b556743b7b2b178365001d
shaojintian <sjt@hnu.edu.cn>
2019-12-10 21:54:14 +0800

    uodate router

--- src/index.ts
+++ src/index.ts
@@ -5,17 +5,31 @@
 
 import Vue from 'vue'
 import ElementUI from 'element-ui';
+import Router from "vue-router";
 import 'element-ui/lib/theme-chalk/index.css';
+import APP from "./APP.vue"
 import topHeader from './components/topHeader.vue'
 import training from './components/training.vue'
 
+Vue.use(Router);
 Vue.use(ElementUI);
 
- new Vue({
-    el:'.app', 
-    components:{
-        topHeader,
-        training,
-    },
-    template: `<topHeader/> `
-})
+const myRouter =  new Router({
+     
+    routes:[
+        {
+            path:'/',
+            components:{
+                topHeader,
+                training,
+            }
+        }
+    ]
+ })
+
+
+new Vue({
+     el:'.app',
+     myRouter,
+     render: h =>h(APP)
+ })
